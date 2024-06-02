9f40924722e55ec63a14a33ced0838a69dd22651
LheanVillarin <168163120+LheanVillarin@users.noreply.github.com>
2024-06-02 16:53:12 +0800

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,45 +1,19 @@
 contract MyToken {
-    string public name = "MyToken";
-    string public symbol = "MTK";
-    uint256 public totalSupply;
-    mapping(address => uint256) public balances;
+    string public name = "MyToken"; // Token name
+    string public symbol = "MTK";   // Token abbreviation
+    uint256 public totalSupply;     // Total supply of tokens
+    mapping(address => uint256) public balances; // Mapping to store balances of addresses
 
     function mint(address _to, uint256 _value) public {
-        totalSupply += _value;
-        balances[_to] += _value;
+        require(_to != address(0), "Invalid address");
+        totalSupply += _value;        // Increase total supply
+        balances[_to] += _value;      // Increase balance of the specified address
     }
 
     function burn(address _from, uint256 _value) public {
+        require(_from != address(0), "Invalid address");
         require(balances[_from] >= _value, "Insufficient balance to burn");
-        totalSupply -= _value;
-        balances[_from] -= _value;
-    }
-}
-
-
-
-
-
-
-
-
-
-contract MyTokenDemo {
-    MyToken myToken = new MyToken();
-
-    function mintDemo() public {
-        myToken.mint(msg.sender, 100);
-    }
-
-    function burnDemo() public {
-        myToken.burn(msg.sender, 50);
-    }
-
-  function viewTotalSupply() public view returns (uint256) {
-        return myToken.totalSupply();
-    }
-
-    function viewBalance() public view returns (uint256) {
-        return myToken.balances(msg.sender);
+        totalSupply -= _value;        // Decrease total supply
+        balances[_from] -= _value;    // Decrease balance of the specified address
     }
 }
